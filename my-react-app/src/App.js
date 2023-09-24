b4052271d4ca384269e9f13db6435d71c9fd2d02
aeiaieo <alexislaineboldt@gmail.com>
2023-09-24 13:13:51 -0700

    React local setup
    Project set up with React and vanilla JS. Styling added for testing purposes.

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import Heading from './components/Heading.js'
+import Footer from './components/Footer.js'
+import Main from './components/Main.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default function Footer(){
+    return (
+        <div>
+            <a><i class="fa-solid fa-user"></i></a>
+        </div>
+    )
+}
